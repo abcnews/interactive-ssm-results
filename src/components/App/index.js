9963daf4a0e017009717d23caedbef900279c49a
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-01 13:52:35 +1000

    Removed a log statement.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -24,7 +24,6 @@ class App extends React.Component {
   fetchData() {
     xhr({ json: true, url: this.props.dataURL }, (err, response, body) => {
       if (!err) {
-        console.log(body);
         this.setState(body);
       }
     });
