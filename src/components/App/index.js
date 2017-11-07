e1cb58e0de838cfacb3e008662d453891bc66cf1
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-07 10:33:38 +1000

    Card component now has a top and bottom prop to separate out children that appear in more/less sections. Added a toggle to show/hide the bottom section.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -35,43 +35,52 @@ class App extends React.Component {
   render() {
     return (
       <div className={styles.root}>
-        <Card id="national">
-          <Text heading={4} headingStyle={3} nomargin>
-            {this.state.title}
-          </Text>
-          <Bar value={this.state.yes / (this.state.yes + this.state.no)} large />
-          <Sides>
+        <Card
+          id="national"
+          top={
             <div>
-              <Percentage value={this.state.yes / (this.state.yes + this.state.no)} yes large />
-              <Integer value={this.state.yes} units="votes" yes large />
+              <Text heading={4} headingStyle={3} nomargin>
+                {this.state.title}
+              </Text>
+              <Bar value={this.state.yes / (this.state.yes + this.state.no)} large />
+              <Sides>
+                <div>
+                  <Percentage value={this.state.yes / (this.state.yes + this.state.no)} yes large />
+                  <Integer value={this.state.yes} units="votes" yes large />
+                </div>
+                <div>
+                  <Percentage value={this.state.no / (this.state.yes + this.state.no)} no large />
+                  <Integer value={this.state.no} units="votes" no large />
+                </div>
+              </Sides>
+              <Share target="national" />
             </div>
+          }
+          bottom={
             <div>
-              <Percentage value={this.state.no / (this.state.yes + this.state.no)} no large />
-              <Integer value={this.state.no} units="votes" no large />
+              <Text heading={4} align={'center'}>
+                Who returned their ballot?
+              </Text>
+              <WaffleGrid
+                waffles={[
+                  { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
+                  { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
+                  { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
+                  { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
+                  { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
+                  { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
+                  { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
+                  { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
+                  { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no }
+                ]}
+              />
+              <Text>
+                Stuff about what we think will happen in parliament. There is such a lot of talk going around about
+                branding, but how do you use it to help you reach more people and market your products or services?
+              </Text>
             </div>
-          </Sides>
-          <Share target="national" />
-          <Text heading={4} align={'center'}>
-            Who returned their ballot?
-          </Text>
-          <WaffleGrid
-            waffles={[
-              { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
-              { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
-              { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
-              { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
-              { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
-              { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
-              { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no },
-              { label: 'No', value: this.state.no, total: this.state.yes + this.state.no },
-              { label: 'Yes', value: this.state.yes, total: this.state.yes + this.state.no }
-            ]}
-          />
-          <Text>
-            Stuff about what we think will happen in parliament. There is such a lot of talk going around about
-            branding, but how do you use it to help you reach more people and market your products or services?
-          </Text>
-        </Card>
+          }
+        />
       </div>
     );
   }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,34 @@
 const React = require('react');
 const styles = require('./styles.scss');
 
-const Card = ({ id, children }) => (
-  <div id={id} className={styles.root}>
-    {children}
-  </div>
-);
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: window.location.hash.split('#') === this.props.id
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} className={styles.root}>
+        <button className={styles[this.state.isOpen ? 'less' : 'more']} onClick={this.toggle}>
+          {this.state.isOpen ? 'less' : 'more'}
+        </button>
+        {this.props.top}
+        {this.state.isOpen && this.props.bottom}
+      </div>
+    );
+  }
+}
 
 module.exports = Card;
