a7494c373d8a9818630b376da9f51640f240c766
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-09 09:51:23 +1000

    Created States component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,11 @@
 const PropTypes = require('prop-types');
 const React = require('react');
 const xhr = require('xhr');
-const { adjective } = require('../../util');
-const Abbreviation = require('../Abbreviation');
 const Card = require('../Card');
-const CardGrid = require('../CardGrid');
 const National = require('../National');
 const Result = require('../Result');
 const Share = require('../Share');
+const States = require('../States');
 const Text = require('../Text');
 const Turnout = require('../Turnout');
 const styles = require('./styles.scss');
@@ -49,25 +47,7 @@ class App extends React.Component {
           reps={this.state.houses[0]}
           senate={this.state.houses[1]}
         />
-        <CardGrid
-          cards={this.getElectoratesForLevel('state').map(electorate => ({
-            id: electorate.electorate_id,
-            top: [
-              <Text key="heading" heading={4} nomargin>
-                <Abbreviation title={electorate.electorate_name}>{electorate.electorate_name_short}</Abbreviation>
-              </Text>,
-              <Result key="result" electorate={electorate} bar percentage />
-            ],
-            middle: <Result electorate={electorate} integer />,
-            bottom: [
-              <Share key="share" target={electorate.electorate_id} />,
-              <Turnout key="turnout" electorate={electorate} />,
-              <Text key="heading" heading={4} align="center">
-                {`Which ${adjective(electorate.electorate_name)} senators are likely to support same-sex marriage?`}
-              </Text>
-            ]
-          }))}
-        />
+        <States electorates={this.getElectoratesForLevel('state')} />
         {this.getElectoratesForLevel('division').map(electorate => (
           <Card
             key={electorate.electorate_id}
---
+++ src/components/States/index.js
@@ -0,0 +1,41 @@
+const PropTypes = require('prop-types');
+const React = require('react');
+const { adjective } = require('../../util');
+const Abbreviation = require('../Abbreviation');
+const CardGrid = require('../CardGrid');
+const Result = require('../Result');
+const Share = require('../Share');
+const Text = require('../Text');
+const Turnout = require('../Turnout');
+
+const States = ({ electorates }) => (
+  <CardGrid
+    cards={electorates.map(electorate => ({
+      id: electorate.electorate_id,
+      top: [
+        <Text key="heading" heading={4} nomargin>
+          <Abbreviation title={electorate.electorate_name}>{electorate.electorate_name_short}</Abbreviation>
+        </Text>,
+        <Result key="result" electorate={electorate} bar percentage />
+      ],
+      middle: <Result electorate={electorate} integer />,
+      bottom: [
+        <Share key="share" target={electorate.electorate_id} />,
+        <Turnout key="turnout" electorate={electorate} />,
+        <Text key="heading" heading={4} align="center">
+          {`Which ${adjective(electorate.electorate_name)} senators are likely to support same-sex marriage?`}
+        </Text>
+      ]
+    }))}
+  />
+);
+
+States.propTypes = {
+  electorates: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+States.defaultProps = {
+  electorate: [{}]
+};
+
+module.exports = States;
