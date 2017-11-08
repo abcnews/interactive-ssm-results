3625269b9e96253cb682475aced0fb07585db819
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-09 09:19:41 +1000

    Refactored components to render arrays instead of placing everything in un-necessary wrapping elements.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -47,86 +47,73 @@ class App extends React.Component {
           <Card
             key={electorate.electorate_id}
             id={electorate.electorate_id}
-            top={
-              <div>
-                <Text heading={4} headingStyle={3} nomargin>
-                  {electorate.electorate_name}
-                </Text>
-                <Result electorate={electorate} bar integer large percentage />
-                <Share target={electorate.electorate_id} />
-              </div>
-            }
-            bottom={
-              <div>
-                <Turnout electorate={electorate} />
-                <Text heading={4} align="center">
-                  So what happens next?
-                </Text>
-                <Text todo>
-                  Stuff about what we think will happen in parliament. There is such a lot of talk going around about
-                  branding, but how do you use it to help you reach more people and market your products or services?
-                </Text>
-                <Text heading={5} headingStyle={4} nomargin>
-                  {this.state.houses[0].house_name}
-                </Text>
-                <Result electorate={this.state.houses[0]} bar integer percentage prediction units={'members'} />
-                <br />
-                <Text heading={5} headingStyle={4} nomargin>
-                  {this.state.houses[1].house_name}
-                </Text>
-                <Result electorate={this.state.houses[1]} bar integer percentage prediction units={'members'} />
-                <Text todo>
-                  More stuff about what we think will happen in parliament. There is such a lot of talk going around
-                  about branding, but how do you use it to help you reach more people and market your products or
-                  services?
-                </Text>
-              </div>
-            }
+            top={[
+              <Text heading={4} headingStyle={3} nomargin>
+                {electorate.electorate_name}
+              </Text>,
+              <Result electorate={electorate} bar integer large percentage />,
+              <Share target={electorate.electorate_id} />
+            ]}
+            bottom={[
+              <Turnout electorate={electorate} />,
+              <Text heading={4} align="center">
+                So what happens next?
+              </Text>,
+              <Text todo>
+                Stuff about what we think will happen in parliament. There is such a lot of talk going around about
+                branding, but how do you use it to help you reach more people and market your products or services?
+              </Text>,
+              <Text heading={5} headingStyle={4} nomargin>
+                {this.state.houses[0].house_name}
+              </Text>,
+              <Result electorate={this.state.houses[0]} bar integer percentage prediction units={'members'} />,
+              <br />,
+              <Text heading={5} headingStyle={4} nomargin>
+                {this.state.houses[1].house_name}
+              </Text>,
+              <Result electorate={this.state.houses[1]} bar integer percentage prediction units={'members'} />,
+              <Text todo>
+                More stuff about what we think will happen in parliament. There is such a lot of talk going around about
+                branding, but how do you use it to help you reach more people and market your products or services?
+              </Text>
+            ]}
           />
         ))}
         <CardGrid
           cards={this.getElectoratesForLevel('state').map(electorate => ({
             id: electorate.electorate_id,
-            top: (
-              <div>
-                <Text heading={4} nomargin>
-                  <Abbreviation title={electorate.electorate_name}>{electorate.electorate_name_short}</Abbreviation>
-                </Text>
-                <Result electorate={electorate} bar percentage />
-              </div>
-            ),
+            top: [
+              <Text heading={4} nomargin>
+                <Abbreviation title={electorate.electorate_name}>{electorate.electorate_name_short}</Abbreviation>
+              </Text>,
+              <Result electorate={electorate} bar percentage />
+            ],
             middle: <Result electorate={electorate} integer />,
-            bottom: (
-              <div>
-                <Share target={electorate.electorate_id} />
-                <Turnout electorate={electorate} />
-                <Text heading={4} align="center">
-                  {`Which ${adjective(electorate.electorate_name)} senators are likely to support same-sex marriage?`}
-                </Text>
-              </div>
-            )
+            bottom: [
+              <Share target={electorate.electorate_id} />,
+              <Turnout electorate={electorate} />,
+              <Text heading={4} align="center">
+                {`Which ${adjective(electorate.electorate_name)} senators are likely to support same-sex marriage?`}
+              </Text>
+            ]
           }))}
         />
         {this.getElectoratesForLevel('division').map(electorate => (
           <Card
             key={electorate.electorate_id}
             id={electorate.electorate_id}
-            top={
-              <div>
-                <Text heading={4} nomargin>
-                  {electorate.electorate_name}
-                  <small>{electorate.state_name}</small>
-                </Text>
-                <Result electorate={electorate} bar percentage />
-              </div>
-            }
-            bottom={
-              <div>
-                <Result electorate={electorate} integer />
-                <Share target={electorate.electorate_id} />
-                <Turnout electorate={electorate} />
-              </div>
-            }
+            top={[
+              <Text heading={4} nomargin>
+                {electorate.electorate_name}
+                <small>{electorate.state_name}</small>
+              </Text>,
+              <Result electorate={electorate} bar percentage />
+            ]}
+            bottom={[
+              <Result electorate={electorate} integer />,
+              <Share target={electorate.electorate_id} />,
+              <Turnout electorate={electorate} />
+            ]}
           />
         ))}
       </div>
--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -5,23 +5,21 @@ const Integer = require('../Integer');
 const Percentage = require('../Percentage');
 const Sides = require('../Sides');
 
-const Result = ({ electorate, bar, prediction, percentage, integer, large, units }) => (
-  <div>
-    {bar && <Bar value={electorate.response_yes_percentage} large={large} prediction={prediction} />}
-    {(percentage || integer) && (
-      <Sides>
-        <div>
-          {percentage && <Percentage value={electorate.response_yes_percentage} yes large={large} />}
-          {integer && <Integer value={electorate.response_yes_count} units={units} yes large={large} />}
-        </div>
-        <div>
-          {percentage && <Percentage value={electorate.response_no_percentage} no large={large} />}
-          {integer && <Integer value={electorate.response_no_count} units={units} no large={large} />}
-        </div>
-      </Sides>
-    )}
-  </div>
-);
+const Result = ({ electorate, bar, prediction, percentage, integer, large, units }) => [
+  bar && <Bar value={electorate.response_yes_percentage} large={large} prediction={prediction} />,
+  (percentage || integer) && (
+    <Sides>
+      <div>
+        {percentage && <Percentage value={electorate.response_yes_percentage} yes large={large} />}
+        {integer && <Integer value={electorate.response_yes_count} units={units} yes large={large} />}
+      </div>
+      <div>
+        {percentage && <Percentage value={electorate.response_no_percentage} no large={large} />}
+        {integer && <Integer value={electorate.response_no_count} units={units} no large={large} />}
+      </div>
+    </Sides>
+  )
+];
 
 Result.propTypes = {
   electorate: PropTypes.object.isRequired,
