79a9b8f4fdeda471df9dc1227f51d4976cbba750
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-09 09:58:49 +1000

    Added Divisions component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,9 @@
 const PropTypes = require('prop-types');
 const React = require('react');
 const xhr = require('xhr');
-const Card = require('../Card');
+const Divisions = require('../Divisions');
 const National = require('../National');
-const Result = require('../Result');
-const Share = require('../Share');
 const States = require('../States');
-const Text = require('../Text');
-const Turnout = require('../Turnout');
 const styles = require('./styles.scss');
 
 class App extends React.Component {
@@ -48,24 +44,7 @@ class App extends React.Component {
           senate={this.state.houses[1]}
         />
         <States electorates={this.getElectoratesForLevel('state')} />
-        {this.getElectoratesForLevel('division').map(electorate => (
-          <Card
-            key={electorate.electorate_id}
-            id={electorate.electorate_id}
-            top={[
-              <Text key="heading" heading={4} nomargin>
-                {electorate.electorate_name}
-                <small>{electorate.state_name}</small>
-              </Text>,
-              <Result key="result" electorate={electorate} bar percentage />
-            ]}
-            bottom={[
-              <Result key="result" electorate={electorate} integer />,
-              <Share key="share" target={electorate.electorate_id} />,
-              <Turnout key="turnout" electorate={electorate} />
-            ]}
-          />
-        ))}
+        <Divisions electorates={this.getElectoratesForLevel('division')} />
       </div>
     );
   }
---
+++ src/components/Divisions/index.js
@@ -0,0 +1,37 @@
+const PropTypes = require('prop-types');
+const React = require('react');
+const Card = require('../Card');
+const Result = require('../Result');
+const Share = require('../Share');
+const Text = require('../Text');
+const Turnout = require('../Turnout');
+
+const Divisions = ({ electorates }) =>
+  electorates.map(electorate => (
+    <Card
+      key={electorate.electorate_id}
+      id={electorate.electorate_id}
+      top={[
+        <Text key="heading" heading={4} nomargin>
+          {electorate.electorate_name}
+          <small>{electorate.state_name}</small>
+        </Text>,
+        <Result key="result" electorate={electorate} bar percentage />
+      ]}
+      bottom={[
+        <Result key="result" electorate={electorate} integer />,
+        <Share key="share" target={electorate.electorate_id} />,
+        <Turnout key="turnout" electorate={electorate} />
+      ]}
+    />
+  ));
+
+Divisions.propTypes = {
+  electorates: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+Divisions.defaultProps = {
+  electorate: [{}]
+};
+
+module.exports = Divisions;
