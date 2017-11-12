4c2a11535e0d089c6058a61091bb1766fe356313
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-13 09:57:12 +1000

    ElectorateFinder can now get results from local and remote (Mapbox) data sources.

--- src/components/ElectorateFinder/index.js
+++ src/components/ElectorateFinder/index.js
@@ -7,13 +7,22 @@ const xhr = require('xhr');
 
 const styles = require('./styles.scss');
 
-const FIND_DEBOUNCE_PERIOD = 500;
-const LEAFLET_SRC = 'https://unpkg.com/leaflet@1.2.0/dist/leaflet.js';
-const LEAFLET_UNDERNEATH_SRC = 'https://unpkg.com/leaflet-underneath@3.0.0/dist/leaflet-underneath.js';
-const MAPBOX_TOKEN = 'pk.eyJ1IjoibmV3cy1vbjFpbmUiLCJhIjoiR3FlZFZlVSJ9._30EFE9XYhQitqf4gzRG-';
+const DEBOUNCE_PERIOD = 500;
+const ELECTORATE_PROPERTY = 'Elect_div';
+const JS_LEAFLET = 'https://unpkg.com/leaflet@1.2.0/dist/leaflet.js';
+const JS_LEAFLET_UNDERNEATH = 'https://unpkg.com/leaflet-underneath@3.0.0/dist/leaflet-underneath.js';
+const LEAFLET_LAT_LNG = { lat: -35.3082, lng: 149.1244 };
+const LEAFLET_ZOOM = 11;
+const LEAFLET_UNDERNEATH_DEFAULT_RADIUS = 1;
+const LEAFLET_UNDERNEATH_FEATURE_ID = feature => feature.properties[ELECTORATE_PROPERTY];
+const LEAFLET_UNDERNEATH_LAYERS = ['ssm-2017-electorates-ao2dkw'];
+const LEAFLET_UNDERNEATH_QUERY_OPTIONS = { onlyInside: true };
+const LEAFLET_UNDERNEATH_SUBDOMAINS = ['a', 'b', 'c', 'd'];
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmV3cy1vbjFpbmUiLCJhIjoiR3FlZFZlVSJ9._30EFE9XYhQitqf4gzRG-g';
+const MAPBOX_GEOCODING_TYPES_ADDRESS = 'address,neighborhood,locality,place';
+const MAPBOX_GEOCODING_TYPES_POSTCODE = 'postcode';
 const MAPBOX_GEOCODING_URL_ROOT = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
-
-const cache = {};
+const MAPBOX_TILE_URL = `https://{s}.tiles.mapbox.com/v4/news-on1ine.1o3dijei/{z}/{x}/{y}.vector.pbf?access_token=${MAPBOX_ACCESS_TOKEN}`;
 
 class ElectorateFinder extends React.Component {
   constructor(props) {
@@ -24,7 +33,7 @@ class ElectorateFinder extends React.Component {
     this.getInputRef = this.getInputRef.bind(this);
     this.getMapRef = this.getMapRef.bind(this);
     this.init = this.init.bind(this);
-    this.find = debounce(this.find.bind(this), FIND_DEBOUNCE_PERIOD);
+    this.find = debounce(this.find.bind(this), DEBOUNCE_PERIOD);
     this.pick = this.pick.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
   }
@@ -38,14 +47,40 @@ class ElectorateFinder extends React.Component {
   }
 
   init() {
-    if (this.map) {
+    if (this.hasMap) {
       return;
     }
 
     loadDependencies(() => {
       this.map = L.map(this.mapEl);
-      // this.mapCenter =
+      this.map.on('load', () => (this.hasMap = true));
+      this.map.setView(L.latLng(LEAFLET_LAT_LNG), LEAFLET_ZOOM);
+      this.underneath = L.underneath(MAPBOX_TILE_URL, this.map, {
+        featureId: LEAFLET_UNDERNEATH_FEATURE_ID,
+        defaultRadius: LEAFLET_UNDERNEATH_DEFAULT_RADIUS,
+        layers: LEAFLET_UNDERNEATH_LAYERS,
+        subdomains: LEAFLET_UNDERNEATH_SUBDOMAINS
+      });
     });
+  }
+
+  electorateAtLatLng(latLng, cb) {
+    this.underneath.query(
+      latLng,
+      (err, results) => {
+        if (!results.length) {
+          return;
+        }
+
+        cb(
+          results
+            .filter((x, index) => index === 0)
+            .map(result => electorateIdFromName(result.properties[ELECTORATE_PROPERTY]))
+        );
+      },
+      null,
+      LEAFLET_UNDERNEATH_QUERY_OPTIONS
+    );
   }
 
   find(event) {
@@ -66,29 +101,29 @@ class ElectorateFinder extends React.Component {
     }
 
     if (query == +query) {
-      return this.findByPostcode(query, (error, results) => this.setState({ results: results || [] }));
+      return this.findByPostcode(query, results => this.setState({ results }));
     }
 
-    this.findByAddress(query, (error, results) => this.setState({ results: results || [] }));
+    this.findByAddress(query, results => this.setState({ results }));
   }
 
   findByAddress(query, cb) {
-    return []; // TODO: Geocoding
+    this.findRemote(query, MAPBOX_GEOCODING_TYPES_ADDRESS, cb);
   }
 
   findByName(query) {
-    return this.findInList(query, this.props.electorates, 'electorate_name');
+    return this.findLocal(query, this.props.electorates, 'electorate_name');
   }
 
   findByMP(query) {
-    return this.findInList(query, this.props.mps, 'politician_name');
+    return this.findLocal(query, this.props.mps, 'politician_name');
   }
 
   findByPostcode(query, cb) {
-    return []; // TODO: Geocoding
+    this.findRemote(query, MAPBOX_GEOCODING_TYPES_POSTCODE, cb);
   }
 
-  findInList(query, list, key) {
+  findLocal(query, list, key) {
     const _query = query.toLowerCase();
 
     return unique(
@@ -101,6 +136,27 @@ class ElectorateFinder extends React.Component {
           return memo.concat([item.electorate_id]);
         }, [])
         .sort()
+    );
+  }
+
+  findRemote(query, types, cb) {
+    fetchJSON(
+      `${MAPBOX_GEOCODING_URL_ROOT}${encodeURIComponent(query)}.json?access_token=${encodeURIComponent(
+        MAPBOX_ACCESS_TOKEN
+      )}&country=au&types=${encodeURIComponent(types)}`,
+      data => {
+        if (!data || !data.features[0]) {
+          return;
+        }
+
+        this.electorateAtLatLng(
+          L.latLng({
+            lat: data.features[0].center[1],
+            lng: data.features[0].center[0]
+          }),
+          cb
+        );
+      }
     );
   }
 
@@ -172,19 +228,44 @@ ElectorateFinder.defaultProps = {
   onElectorateChosen: electorate => console.debug(`Electorate chosen: ${electorate}`)
 };
 
-let isLoaded;
+const electorateIdFromName = name => name.toLowerCase().replace(/[^a-z]/g, '');
 
-const loadDependencies = cb => {
-  if (isLoaded) {
-    return cb();
+const cachedJSON = {};
+
+const fetchJSON = (url, cb) => {
+  if (cachedJSON[url]) {
+    return cb(cachedJSON[url]);
   }
 
-  loadScript(LEAFLET_SRC, () => {
-    loadScript(LEAFLET_UNDERNEATH_SRC, () => {
-      isLoaded = true;
-      cb();
-    });
-  });
+  xhr(
+    {
+      json: true,
+      url
+    },
+    (err, response, body) => {
+      if (err) {
+        return cb();
+      }
+
+      if (!cachedJSON[url]) {
+        cachedJSON[url] = body;
+      }
+
+      cb(cachedJSON[url]);
+    }
+  );
 };
+
+let areDependenciesLoaded;
+
+const loadDependencies = cb =>
+  areDependenciesLoaded
+    ? cb()
+    : loadScript(JS_LEAFLET, () => {
+        loadScript(JS_LEAFLET_UNDERNEATH, () => {
+          areDependenciesLoaded = true;
+          cb();
+        });
+      });
 
 module.exports = ElectorateFinder;
