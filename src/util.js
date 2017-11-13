7f1986fa412d2a59c356951be63fc0d215b89f0a
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-11-13 16:14:51 +1000

    Moved copy-text-to-colipboard package into util as it is written in ES2015 and wasn't transpiled.

--- src/util.js
+++ src/util.js
@@ -9,6 +9,51 @@ module.exports.adjective = text => text.replace(ENDS_IN_IA, IAN);
 module.exports.colloquial = (name, shouldUpcase) =>
   name.indexOf('Territory') > -1 ? `${shouldUpcase ? 'The' : 'the'} ${name}` : name;
 
+module.exports.copyTextToClipboard = input => {
+  // Source (MIT): https://github.com/sindresorhus/copy-text-to-clipboard
+
+  const el = document.createElement('textarea');
+
+  el.value = input;
+
+  // Prevent keyboard from showing on mobile
+  el.setAttribute('readonly', '');
+
+  el.style.contain = 'strict';
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  el.style.fontSize = '12pt'; // Prevent zooming on iOS
+
+  const selection = getSelection();
+
+  let originalRange = false;
+  if (selection.rangeCount > 0) {
+    originalRange = selection.getRangeAt(0);
+  }
+
+  document.body.appendChild(el);
+  el.select();
+
+  // Explicit selection workaround for iOS
+  el.selectionStart = 0;
+  el.selectionEnd = input.length;
+
+  let success = false;
+
+  try {
+    success = document.execCommand('copy');
+  } catch (err) {}
+
+  document.body.removeChild(el);
+
+  if (originalRange) {
+    selection.removeAllRanges();
+    selection.addRange(originalRange);
+  }
+
+  return success;
+};
+
 module.exports.nowrap = text => text.replace(WHITESPACE, NBSP);
 
 module.exports.scrollToId = id =>
